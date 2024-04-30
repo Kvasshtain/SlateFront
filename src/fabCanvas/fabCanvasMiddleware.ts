e93a2a8af38c0f8df813df0cbcf35f2c2c2aa7ad
kvasshtain <kvasko-v@yandex.ru>
2024-04-30 17:40:51 +0400

    Для canvas добавлен zoom

--- src/fabCanvas/fabCanvasMiddleware.ts
+++ src/fabCanvas/fabCanvasMiddleware.ts
@@ -133,6 +133,37 @@ const fabCanvasMiddleware = (): Middleware => {
 
         store.dispatch(sendCanvasObjectModification(objectModificationData))
       })
+
+      canvas.on("mouse:wheel", (opt) => {
+        var vpt = canvas.viewportTransform
+
+        if (!vpt) return
+
+        var delta = opt.e.deltaY
+        var zoom = canvas.getZoom()
+        zoom *= 0.999 ** delta
+        if (zoom > 20) zoom = 20
+        if (zoom < 0.01) zoom = 0.01
+        canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom)
+        opt.e.preventDefault()
+        opt.e.stopPropagation()
+
+        if (zoom < 400 / 1000) {
+          vpt[4] = 200 - (1000 * zoom) / 2
+          vpt[5] = 200 - (1000 * zoom) / 2
+        } else {
+          if (vpt[4] >= 0) {
+            vpt[4] = 0
+          } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
+            vpt[4] = canvas.getWidth() - 1000 * zoom
+          }
+          if (vpt[5] >= 0) {
+            vpt[5] = 0
+          } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
+            vpt[5] = canvas.getHeight() - 1000 * zoom
+          }
+        }
+      })
     }
 
     if (addTextOnCanvas.match(action)) {
